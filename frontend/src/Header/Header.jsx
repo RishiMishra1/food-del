7465ea826a21ae89260c519db8611e7cc6cfa00e
RishiMishra1 <shusmamishra5002@gmail.com>
2025-03-15 18:44:23 +0530

    Added Header component with background image

--- frontend/src/Header/Header.jsx
+++ frontend/src/Header/Header.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import headerImage from '../assets/header_img.png'; // Import the image
 
 function Header() {
   return (
-    <div className='Header'>
+    <div 
+      className='Header' 
+      style={{ backgroundImage: `url(${headerImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
+    >
         <div className="header-contents">
             <h2>Order your Favourite Food here</h2>
-            <p>Choose from a diverse menu featuring a delectable aaray of dishes crafted with the finest ingridients and cuinary expertise .Our mission is to satisfy your cravings and elevate your dining experience,one delicious meal at a time</p>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
             <button>View Menu</button>
         </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
